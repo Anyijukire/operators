a99d46f142ac71f4b055c9cdc31626d9c6f15c09
Anyijukire <79198971+Anyijukire@users.noreply.github.com>
2021-03-01 22:34:21 +0300

    Add files via upload

---
+++ helloo.kt
@@ -0,0 +1,28 @@
+fun main() {
+ println("Hello Janet")
+    division()
+    add()
+    fact()
+}
+fun name(){
+    val name="Janet"
+    println("Hello Janet")
+}
+fun division(){
+    var num1=22
+    var num2=6
+    var modulus=num1%num2
+    println(modulus)
+}
+fun add(){
+    var num1=22
+    var num2=41
+    var num3=50
+    var num4=43
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun fact(){
+    var fact=("Janet is so social")
+    println(fact)
+}
